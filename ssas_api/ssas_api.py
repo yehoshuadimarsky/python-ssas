aa03f4cba97fc7520efe6d40de54792f2c88e403
Josh Dimarsky <24758845+yehoshuadimarsky@users.noreply.github.com>
2019-12-28 19:42:42 -0500

    Major performance enhancements, improved overall code
    Also formatted with Black

--- ssas_api/ssas_api.py
+++ ssas_api/ssas_api.py
@@ -5,79 +5,81 @@ Created on Wed Sep 20 16:59:43 2017
 @author: Yehoshua
 """
 
-import os
-import sys
-
 import pandas as pd
-import clr # name for pythonnet
+import numpy as np
 
-# logging portion
+from pathlib import Path
 import logging
+
 logger = logging.getLogger(__name__)
 
-           
-def _load_assemblies():
-    '''Loads required assemblies, called after function definition.'''
+try:
+    import clr  # name for pythonnet
+except ImportError:
+    msg = """
+    Could not import 'clr', install the 'pythonnet' library. 
+    For conda, `conda install -c pythonnet pythonnet`
+    """
+    raise ImportError(msg)
 
-    # Full path of .dll files, gets newest version if there are several folders
-    amo_root = (r"C:\Windows\Microsoft.NET\assembly\GAC_MSIL"
-                r"\Microsoft.AnalysisServices.Tabular")
-    amo_folder = max(os.listdir(amo_root))
-    amo_path = os.path.join(amo_root,amo_folder,
-                            os.listdir(os.path.join(amo_root,amo_folder))[0]
-                            )
-    
-    adomd_root = (r"C:\Windows\Microsoft.NET\assembly\GAC_MSIL"
-                  r"\Microsoft.AnalysisServices.AdomdClient")
-    adomd_folder = max(os.listdir(adomd_root))
-    adomd_path = os.path.join(adomd_root, adomd_folder,
-                              os.listdir(os.path.join(adomd_root,adomd_folder))[0]
-                              )
-    
-    # Check that file paths are valid
-    assert os.path.isfile(amo_path), ('The filepath for the AMO dll is '
-                         'invalid , you passed the filepath: {}'.format(amo_path))
-    assert os.path.isfile(adomd_path), ('The filepath for the ADOMD dll is '
-                         'invalid , you passed the filepath: {}'.format(adomd_path))
+
+def _load_assemblies():
+    """
+    Loads required assemblies, called after function definition.
+    Might need to install SSAS client libraries:
+    https://docs.microsoft.com/en-us/azure/analysis-services/analysis-services-data-providers
+    """
+    # Full path of .dll files
+    root = Path(r"C:\Windows\Microsoft.NET\assembly\GAC_MSIL")
+    # get latest version of libraries if multiple libraries are installed (max func)
+    amo_path = str(
+        max((root / "Microsoft.AnalysisServices.Tabular").iterdir())
+        / "Microsoft.AnalysisServices.Tabular.dll"
+    )
+    adomd_path = str(
+        max((root / "Microsoft.AnalysisServices.AdomdClient").iterdir())
+        / "Microsoft.AnalysisServices.AdomdClient.dll"
+    )
 
     # load .Net assemblies
-    logger.info('Loading .Net assemblies...')
-    clr.AddReference('System')
-    clr.AddReference('System.Data')
+    logger.info("Loading .Net assemblies...")
+    clr.AddReference("System")
+    clr.AddReference("System.Data")
     clr.AddReference(amo_path)
     clr.AddReference(adomd_path)
-    
+
     # Only after loaded .Net assemblies
     global System, DataTable, AMO, ADOMD
-    
+
     import System
     from System.Data import DataTable
     import Microsoft.AnalysisServices.Tabular as AMO
     import Microsoft.AnalysisServices.AdomdClient as ADOMD
-    logger.info('Successfully loaded these .Net assemblies: ')
 
-    # log each assembly loaded
+    logger.info("Successfully loaded these .Net assemblies: ")
     for a in clr.ListAssemblies(True):
-        logger.debug(a.split(',')[0])
-    
+        logger.info(a.split(",")[0])
+
 
 _load_assemblies()
 
 
-def set_conn_string(ssas_server,db_name,username,password):
-    '''
-    Sets connection string to SSAS database, in this case designed for Azure Analysis Services
-    '''
+def set_conn_string(server, db_name, username, password):
+    """
+    Sets connection string to SSAS database, 
+    in this case designed for Azure Analysis Services
+    """
     conn_string = (
-            'Provider=MSOLAP;Data Source={};Initial Catalog={};User ID={};'
-            'Password={};Persist Security Info=True;Impersonation Level=Impersonate'
-            .format(ssas_server,db_name,username,password)
+        "Provider=MSOLAP;Data Source={};Initial Catalog={};User ID={};"
+        "Password={};Persist Security Info=True;Impersonation Level=Impersonate".format(
+            server, db_name, username, password
+        )
     )
     return conn_string
 
 
-def get_DAX(connection_string,dax_string,remove_brackets=False):
-    '''
+def get_DAX(connection_string, dax_string):
+    """
     Executes DAX query and returns the results as a pandas DataFrame
     
     Parameters
@@ -86,59 +88,79 @@ def get_DAX(connection_string,dax_string,remove_brackets=False):
         Valid SSAS connection string, use the set_conn_string() method to set
     dax_string : string
         Valid DAX query, beginning with EVALUATE or VAR or DEFINE
-    remove_brackets : boolean
-        If True, then removes brackets from column names
 
     Returns
     ----------------
     pandas DataFrame with the results
-    '''
-    dataadapter = ADOMD.AdomdDataAdapter(dax_string,connection_string)
-    table = DataTable()
-    logger.info('Getting DAX query...')
-    try:
-        dataadapter.Fill(table)
-    except Exception as ex:
-        logger.exception('Exception occured:\n')
-        sys.exit(1)
-    
-    col_names = []
-    for c in table.Columns.List:
-        col_names.append(c.ColumnName)
-
-    num_rows = table.Rows.Count
-    
-    d = {}
-    
-    for r in range(num_rows):
-        row_dict = {}
-        for c in table.Columns.List:
-            if isinstance(table.Rows[r][c],System.DBNull):
-                row_dict[c.ColumnName] = None
-            elif isinstance(table.Rows[r][c],System.DateTime):
-                row_dict[c.ColumnName] = pd.Timestamp(table.Rows[r][c].ToShortDateString())
-            else:
-                row_dict[c.ColumnName] = table.Rows[r][c]
-        d[r] = row_dict
-    df = pd.DataFrame.from_dict(d,orient='index')
-
-    # remove brackets from column names
-    if remove_brackets:
-        def remove_brackets(col):
-            x = col
-            if x[0] == '[':
-                x = x.replace('[','')
-            x = x.replace('[','_')
-            x = x.replace(']','')
-            return x
-        df = df.rename(columns=remove_brackets)        
-    logger.info('DAX query successfully retrieved')
+    """
+    table = _get_DAX(connection_string, dax_string)
+    df = _parse_DAX_result(table)
     return df
 
 
-def process_database(connection_string,db_name,refresh_type='full'):
-    '''
-    Processes SSAS data model to get new data from underlying database.
+def _get_DAX(connection_string, dax_string) -> DataTable:
+    dataadapter = ADOMD.AdomdDataAdapter(dax_string, connection_string)
+    table = DataTable()
+    logger.info("Getting DAX query...")
+    dataadapter.Fill(table)
+    logger.info("DAX query successfully retrieved")
+    return table
+
+
+def _parse_DAX_result(table: DataTable) -> pd.DataFrame:
+    cols = [c for c in table.Columns.List]
+    rows = []
+    # much better performance to just access data by position instead of name
+    # and then add column names afterwards
+    for r in range(table.Rows.Count):
+        row = [table.Rows[r][c] for c in cols]
+        rows.append(row)
+
+    df = pd.DataFrame.from_records(rows, columns=[c.ColumnName for c in cols])
+
+    # replace System.DBNull with None
+    # df.replace({System.DBNull: np.NaN}) doesn't work for some reason
+    df = df.applymap(lambda x: np.NaN if isinstance(x, System.DBNull) else x)
+
+    # convert datetimes
+    dt_types = [c.ColumnName for c in cols if c.DataType.FullName == "System.DateTime"]
+    if dt_types:
+        for dtt in dt_types:
+            # if all nulls, then pd.to_datetime will fail
+            if not df.loc[:, dtt].isna().all():
+                ser = df.loc[:, dtt].map(lambda x: x.ToString())
+                df.loc[:, dtt] = pd.to_datetime(ser)
+
+    # convert other types
+    types_map = {"System.Int64": int, "System.Double": float, "System.String": str}
+    col_types = {c.ColumnName: types_map.get(c.DataType.FullName, "object") for c in cols}
+    df = df.astype(col_types)
+
+    return df
+
+
+def process_database(connection_string, refresh_type, db_name):
+    process_model(
+        connection_string=connection_string,
+        item_type="model",
+        refresh_type=refresh_type,
+        db_name=db_name,
+    )
+
+
+def process_table(connection_string, table_name, refresh_type, db_name):
+    process_model(
+        connection_string=connection_string,
+        item_type="table",
+        item=table_name,
+        refresh_type=refresh_type,
+        db_name=db_name,
+    )
+
+
+def process_model(connection_string, db_name, refresh_type="full", item_type="model", item=None):
+    """
+    Processes SSAS data model to get new data from underlying source.
     
     Parameters
     -------------
@@ -148,24 +170,35 @@ def process_database(connection_string,db_name,refresh_type='full'):
         The data model on the SSAS server to process
     refresh_type : string, default `full`
         Type of refresh to process. Currently only supports `full`.
-    '''
-    #connect to the AS instance from Python
+    item_type : string, choice of {'model','table'}, default 'model'
+    item : string, optional.
+        Then name of the item. Only needed when item_type is 'table', to specify the table name
+    """
+    assert item_type.lower() in ("table", "model"), f"Invalid item type: {item_type}"
+    if item_type.lower() == "table" and not item:
+        raise ValueError("If item_type is table, must supply an item (a table name) to process")
+
+    # connect to the AS instance from Python
     AMOServer = AMO.Server()
-    logger.info('Connecting to database...')
+    logger.info("Connecting to database...")
     AMOServer.Connect(connection_string)
-    
-    # Dict of refresh types, I only needed `full`, can add more
-    refresh_dict = {'full': AMO.RefreshType.Full}
-    
+
+    # Dict of refresh types
+    refresh_dict = {"full": AMO.RefreshType.Full}
+
+    # process
     db = AMOServer.Databases[db_name]
-    logger.info('Processing database with refresh type "{}"...'
-                .format(refresh_type))
-    db.Model.RequestRefresh(refresh_dict[refresh_type])
+
+    if item_type.lower() == "table":
+        table = db.Model.Tables.Find(item)
+        table.RequestRefresh(refresh_dict[refresh_type])
+    else:
+        db.Model.RequestRefresh(refresh_dict[refresh_type])
+
     op_result = db.Model.SaveChanges()
     if op_result.Impact.IsEmpty:
-        logger.info('No objects affected by the refresh')
-    
-    logger.info('Disconnecting from Database...')
+        logger.info("No objects affected by the refresh")
+
+    logger.info("Disconnecting from Database...")
     # Disconnect
     AMOServer.Disconnect()
-    
