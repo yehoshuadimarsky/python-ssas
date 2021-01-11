c516dee70251be5b587726996131aa5a3ea0058a
Alessandro Losi <a.losi@biware.it>
2021-01-11 14:07:54 +0100

    fixed DateTime parsing in non-US locale
    The fix consists in forcing the formatted datetime to reflect the ISO 8601 format. This should avoid switching between days and months in locale that use DD/MM/YYYY formats

--- ssas_api.py
+++ ssas_api.py
@@ -174,7 +174,7 @@ def _parse_DAX_result(table: "DataTable") -> pd.DataFrame:
         for dtt in dt_types:
             # if all nulls, then pd.to_datetime will fail
             if not df.loc[:, dtt].isna().all():
-                ser = df.loc[:, dtt].map(lambda x: x.ToString())
+                ser = df.loc[:, dtt].map(lambda x: x.ToString('s'))
                 df.loc[:, dtt] = pd.to_datetime(ser)
 
     # convert other types
